05f2108643b33e8c875634aa542ee6d92938d414
Jason852 <62583852+Jason852@users.noreply.github.com>
2023-05-17 16:31:00 -0400

    working on styling date

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -34,6 +34,30 @@ const DUMMY_EXPENSES = [
     amount: 18.59,
     date: new Date("2022-02-18"),
   },
+  {
+    id: "e6",
+    description: "A pair of trousers",
+    amount: 89.29,
+    date: new Date("2021-12-01"),
+  },
+  {
+    id: "e7",
+    description: "Some of bananas",
+    amount: 5.99,
+    date: new Date("2021-12-01"),
+  },
+  {
+    id: "e8",
+    description: "A book",
+    amount: 14.99,
+    date: new Date("2022-02-19"),
+  },
+  {
+    id: "e9",
+    description: "Another book",
+    amount: 18.59,
+    date: new Date("2022-02-18"),
+  },
 ];
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
@@ -50,7 +74,8 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
+    paddingHorizontal: 24,
+    paddingTop: 24,
     backgroundColor: GlobalStyles.colors.primary700,
   },
 });
